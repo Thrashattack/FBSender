300f35693be2db4bd08706db03287c1aecd46228
Thrashattackm <henrique2092@gmail.com>
2019-11-08 16:41:41 -0400

    removida rota de informação

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,7 +1,7 @@
 module.exports = app => {
     app.route('/')
-        .get(app.src.controller.controller.get)
-    app.route('/v1/sender')
         .post(app.src.controller.controller.post)
-        .get(app.src.controller.controller.info)
+    // app.route('/v1/sender')
+    //     .post(app.src.controller.controller.post)
+    //     .get(app.src.controller.controller.info)
 }
